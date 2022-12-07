6e8ee2d642f99dd76cf6a01fba5b5d8a887807fa
AramWondergem <aramwondergem@gmail.com>
2022-12-07 15:53:09 +0100

    basic functionalities are in the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,76 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+import PokemonCard from "./pokemonCard/PokemonCard";
+import computerSaysNo from "./assets/computers-says-no.gif"
+import useFetch from "./customHooks/useFetch";
+
 
 function App() {
-  return (
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [errorLoading, setErrorLoading] = useState(false)
+
+    async function fetchData ( url = "https://pokeapi.co/api/v2/pokemon" ) {
+        setLoading(true)
+        try {
+            const pokemonData = await axios.get(url)
+
+            console.log(pokemonData)
+
+            setData(pokemonData.data);
+
+        }
+        catch (e) {
+
+            setLoading(false)
+
+            console.error(e.message)
+
+            setErrorLoading(true)
+
+        }
+
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+
+
+
+
+
+    return (
     <div>
         <img src="" alt=""/>
         <div>
-            <button>Vorige</button>
-            <button>Volgende</button>
+            <button disabled={data.previous === null} onClick={ () => fetchData(data.previous)}>Vorige</button>
+            <button disabled={data.next === null} onClick={ () => fetchData(data.next)}>Volgende</button>
         </div>
         <div>
-            <PokemonCard/>
+            {
+                loading &&
+                <p>🏃‍♀️The elves are running hard at the moment to fetch your pokemons 🏃‍♂️</p>
+            }
+            {Object.keys(data).length > 0 &&
+                data.results.map((item) => <PokemonCard key={item.url} url={item.url}/>)
+
+            }
+            {
+                errorLoading &&
+
+                <>
+
+                <img src={computerSaysNo} alt="GIF of computer says no scene of little britian"/>
+
+                <p>Something went wrong when loading the page. Maybe you can refresh the page</p>
+
+                </>
+
+            }
         </div>
     </div>
   );
--- src/pokemonCard/PokemonCard.js
+++ src/pokemonCard/PokemonCard.js
@@ -1,16 +1,72 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import pickachuGif from "../assets/Pikachu_walks.gif"
+import useFetch from "../customHooks/useFetch";
 
-function PokemonCard({name, src, numberOfMoves, weight, abillities}) {
+function PokemonCard({url}) {
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    console.log(url)
+    async function fetchData() {
+        setLoading(true);
+
+        try {
+            const pokemonData = await axios.get(url)
+
+            console.log(pokemonData)
+
+            setData(pokemonData.data);
+
+        } catch (e) {
+
+            console.error(e.message)
+
+        }
+        setLoading(false);
+
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+
+    //
+    // useEffect(() => {
+    //     if (Object.keys(data).length > 0) {
+    //         setLoading(false);
+    //     }
+    //
+    //
+    //     // const {name, moves, weight, abilities, sprites: {front_shiny}} = data.data;
+    //
+    // }, [data])
+
     return (
         <div>
-            <h2>{name}</h2>
-            <img src={src} alt={name}/>
-            <h3>`Moves: ${numberOfMoves}`</h3>
-            <h3>`Weight: ${weight}`</h3>
-            <h3>Abilities</h3>
-            <ul> {
-                // abillities.map() //afmaken na data bekijken
-            }</ul>
+
+            {loading &&
+                <>
+                    <img src={pickachuGif} alt="walking Pikachu"/>
+                    <p>Look, Pikachu is collecting the pokemons</p>
+                </>
+            }
+            {Object.keys(data).length > 0 &&
+                <>
+                    <h2>{data.name}</h2>
+                    <img src={data.sprites.front_shiny} alt={data.name}/>
+                    <h3>Moves: {data.moves.length}</h3>
+                    <h3>Weight: {data.weight}</h3>
+                    <h3>Abilities</h3>
+                    <ul> {
+                        data.abilities.map((ability) => <li key={`${data.name}-${ability.ability.name}`}>{ability.ability.name}</li>)
+                    }</ul>
+                </>
+            }
+
+
         </div>
     );
 }
